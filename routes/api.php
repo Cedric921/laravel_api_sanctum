f789c66e1befdcee172dd1cf90bdb7631d5fd806
cedric karungu <ckarungu921@gmail.com>
2022-03-03 21:17:16 +0200

    add log out route and function

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 
 
-Route::post('/register',  [AuthController::class, 'register']);
 // Route::resource('products', ProductController::class);
+Route::post('/register',  [AuthController::class, 'register']);
 Route::get('/products',  [ProductController::class, 'index']);
 Route::get('/products/{id}',  [ProductController::class, 'show']);
 Route::get('products/search/{name}',  [ProductController::class, 'search']);
@@ -20,6 +20,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
    Route::post('/products',  [ProductController::class, 'store']);
    Route::put('/products/{id}',  [ProductController::class, 'update']);
    Route::delete('/products/{id}',  [ProductController::class, 'destroy']);
+   Route::post('/logout',  [AuthController::class, 'logout']);
 });
 
  Route::get('contacts',[ContactController::class,'contact']);
