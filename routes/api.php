cd99beb95bd89f13e509f48c6984e53eda843ed1
cedric karungu <ckarungu921@gmail.com>
2022-03-03 21:24:16 +0200

    add login

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProductController;
 
 // Route::resource('products', ProductController::class);
 Route::post('/register',  [AuthController::class, 'register']);
+Route::post('/login',  [AuthController::class, 'login']);
 Route::get('/products',  [ProductController::class, 'index']);
 Route::get('/products/{id}',  [ProductController::class, 'show']);
 Route::get('products/search/{name}',  [ProductController::class, 'search']);
