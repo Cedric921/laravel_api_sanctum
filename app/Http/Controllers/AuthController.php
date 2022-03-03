d8d8d5c9ab34507653d51fee31f0492074bbaea9
cedric karungu <ckarungu921@gmail.com>
2022-03-03 21:29:05 +0200

    fix missing on  login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
 
     public function login(Request $request){
         $fields = $request->validate([
-            'email' => 'required|string|unique:users,email',
+            'email' => 'required|string',
             'password' => 'required|string'
         ]);
         
