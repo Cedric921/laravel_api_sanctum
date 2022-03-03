95111b9f1aaf504c001ef58bee15faccb8d6f9b6
cedric karungu <ckarungu921@gmail.com>
2022-03-03 14:21:37 +0200

    add the AuthController

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,18 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 
 
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
+Route::get('/products',  [ProductController::class, 'index']);
+Route::get('/products/{id}',  [ProductController::class, 'show']);
 Route::get('products/search/{name}',  [ProductController::class, 'search']);
  
 // Route::get('/products',  [ProductController::class, 'index']);
 // Route::post('/products',  [ProductController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+   Route::post('/products',  [ProductController::class, 'store']);
+   Route::put('/products/{id}',  [ProductController::class, 'update']);
+   Route::delete('/products/{id}',  [ProductController::class, 'destroy']);
 });
 
  Route::get('contacts',[ContactController::class,'contact']);
